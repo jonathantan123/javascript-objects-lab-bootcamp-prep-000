05bd09a57961079c74f7609984f2829eb14180bd
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-06 13:45:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var recipes = {
-  grain: "water"}
+var recipes = []
 
-function  updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key] : value})
-  
-} 
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key]=value
-  return object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { 
+  object[key] = value
+  return object 
 }
 
+
 function deleteFromObjectByKey(object, key) {
-  var newObject= Object.assign({},object)
+  var newObject= Object.assign( {}, Object)
   delete newObject[key]
-  return  newObject
+  return Object
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign ({}, object, {[key] :value})
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
-  return object
-}
+  return object }
+  
+  
